69654180ae0d8d8a4bd59629aaad333072a77b65
Said oulhaj <oulhajsaid@gmail.com>
2025-11-19 02:45:06 +0100

    refactor: Replace Save and Close with Cancel button in policy forms
    - Remove save_and_close submit button from PolicyType form
    - Update PolicyMenuBuilder to use cancel button instead of save_and_close
    - Cancel button links to list (create) or view (edit) page
    - Remove save_and_close logic from PolicyAdminController
    - Update create and edit templates to use cancel_url parameter
    - Fix template extends to use layout.html.twig
    - Use edit_header with context menu following Ibexa patterns
    
    Benefits:
    - Cleaner UX with Save + Cancel instead of Save + Save and Close
    - Follows standard Ibexa admin patterns
    - Cancel button in context menu for better accessibility
    - Consistent with other Ibexa admin forms

--- src/Form/Type/PolicyType.php
+++ src/Form/Type/PolicyType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -104,6 +105,10 @@ class PolicyType extends AbstractType
                 'attr' => [
                     'class' => 'ibexa-input ibexa-input--checkbox',
                 ],
+                'translation_domain' => 'masilia_consent',
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'policy.form.save',
                 'translation_domain' => 'masilia_consent',
             ]);
     }
---
+++ src/Menu/PolicyMenuBuilder.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Masilia\ConsentBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+
+class PolicyMenuBuilder
+{
+    public function __construct(
+        private readonly FactoryInterface $factory
+    ) {
+    }
+
+    /**
+     * Create menu for policy create/edit sidebar
+     */
+    public function createPolicySidebarMenu(array $options = []): ItemInterface
+    {
+        $menu = $this->factory->createItem('root');
+
+        $saveId = $options['save_id'] ?? null;
+        $cancelUrl = $options['cancel_url'] ?? null;
+
+        if ($saveId) {
+            $menu->addChild('save', [
+                'label' => 'policy.form.save',
+                'extras' => [
+                    'icon' => 'checkmark',
+                    'orderNumber' => 10,
+                ],
+                'attributes' => [
+                    'class' => 'ibexa-btn--trigger',
+                    'data-click' => sprintf('#%s', $saveId),
+                ],
+            ]);
+        }
+
+        if ($cancelUrl) {
+            $menu->addChild('cancel', [
+                'label' => 'common.cancel',
+                'uri' => $cancelUrl,
+                'extras' => [
+                    'icon' => 'circle-close',
+                    'orderNumber' => 20,
+                ],
+                'attributes' => [
+                    'class' => 'ibexa-btn--secondary',
+                ],
+            ]);
+        }
+
+        return $menu;
+    }
+}
